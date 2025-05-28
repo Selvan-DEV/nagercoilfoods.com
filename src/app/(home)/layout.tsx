6444034926b96591296ca53221dfd29f328402de
Selvan <69357513+Selvan-DEV@users.noreply.github.com>
2025-05-28 21:42:24 +0530

    fb header code added

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import { ReactNode } from "react";
 import "../globals.css";
 import { Outfit } from "next/font/google";
@@ -5,6 +6,7 @@ import AppLayoutComponent from "@/components/site-components/AppLayout/AppLayout
 import GlobalLoader from "@/components/shared/GlobalLoader";
 import MotionWrapper from "@/components/site-components/common/MotionWrapper";
 import { ToastContainer } from "react-toastify";
+import Head from "next/head";
 
 const outfit = Outfit({
   subsets: ["latin"],
@@ -22,6 +24,35 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={outfit.variable}>
+      <Head>
+        {/* Meta Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1120531910101112');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            alt="fb"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1120531910101112&ev=PageView&noscript=1"
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
+      </Head>
       <body className={outfit.className}>
         <GlobalLoader />
         <AppLayoutComponent>
